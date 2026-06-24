c3f50b4acb4921e314feff5781c9b30020eef040
Softolic <softolic.twt@gmail.com>
2026-06-23 21:51:06 -0300

    Ajustes na movimentaçao

--- Assets/Scripts/PlayerControle.cs
+++ Assets/Scripts/PlayerControle.cs
@@ -22,12 +22,9 @@ public class PlayerControle : MonoBehaviour
     void look()
     {
 
-        var matrix = Matrix4x4.Rotate(Quaternion.EuLer(0,45,0));
-
-        var inputtorto = matrix.MultiplyPoint3x4(_input);
-
+     
         if(_input != Vector3.zero){
-        var relative = (transform.position + inputtorto) - transform.position;
+        var relative = (transform.position + _input.ToIso()) - transform.position;
         var rotation = Quaternion.LookRotation(relative, Vector3.up);
 
         transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, _turnSpeed * Time.deltaTime);
