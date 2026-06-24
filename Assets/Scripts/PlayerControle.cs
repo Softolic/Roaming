dbfe9ec70b65682727249297d1d315d32c012ec7
Softolic <softolic.twt@gmail.com>
2026-06-23 21:34:50 -0300

    Movimentaçao, reta em jogo torto

--- Assets/Scripts/PlayerControle.cs
+++ Assets/Scripts/PlayerControle.cs
@@ -4,6 +4,7 @@ public class PlayerControle : MonoBehaviour
 {
     [SerializeField] private Rigidbody _rb;
     [SerializeField] private float _speed = 5;
+    [SerializeField] private float _turnSpeed = 360;
        private Vector3 _input;
        void Update()
     {
@@ -20,13 +21,20 @@ public class PlayerControle : MonoBehaviour
     }
     void look()
     {
-        var relative = (transform.position + _input) - transform.position;
+
+        var matrix = Matrix4x4.Rotate(Quaternion.EuLer(0,45,0));
+
+        var inputtorto = matrix.MultiplyPoint3x4(_input);
+
+        if(_input != Vector3.zero){
+        var relative = (transform.position + inputtorto) - transform.position;
         var rotation = Quaternion.LookRotation(relative, Vector3.up);
 
-        transform.rotation = rotation;
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, _turnSpeed * Time.deltaTime);
+    }
     }
 
     void Move() {
-        _rb.MovePosition(transform.position + transform.forward * _speed * Time.deltaTime);
+        _rb.MovePosition(transform.position + (transform.forward * _input.magnitude) * _speed * Time.deltaTime);
     }
 }
